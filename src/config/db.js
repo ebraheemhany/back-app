a4b957ffc7798b37560fca432d9f79838aa93608
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-01 16:24:40 +0300

    fix db connection for railway

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "social_app",
-  password: "2004",
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        // ✅ Railway
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        // ✅ Local
+        user: "postgres",
+        host: "localhost",
+        database: "social_app",
+        password: "2004",
+        port: 5432,
+      }
+);
 
 pool
   .connect()
