e39a0d94a6b1ca51ed991c70ba5ec8915ecd5eb8
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-02 14:01:43 +0300

    fix cros seting

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
+    origin: "http://localhost:3000",
+    credentials: true,
   },
 });
 
