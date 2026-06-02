057b5bbc3ebaff73eb2845ffd881443e849d0698
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-02 22:44:03 +0300

    useing pravo liberary2

--- src/config/email.service.js
+++ src/config/email.service.js
@@ -1,22 +1,20 @@
-const Brevo = require("@getbrevo/brevo");
+const SibApiV3Sdk = require("@getbrevo/brevo");
 
-const client = Brevo.ApiClient.instance;
-client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
-
-const apiInstance = new Brevo.TransactionalEmailsApi();
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+apiInstance.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
 
 const sendOTPEmail = async (email, otp) => {
-  const sendSmtpEmail = {
-    to: [{ email }],
-    sender: { email: "alihasan666.co@gmail.com", name: "App" },
-    subject: "Reset Password OTP",
-    htmlContent: `
-      <h2>Reset Password</h2>
-      <p>الـ OTP بتاعك هو:</p>
-      <h1 style="color: #4CAF50; letter-spacing: 5px">${otp}</h1>
-      <p>صالح لمدة 10 دقايق بس</p>
-    `,
-  };
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+  sendSmtpEmail.to = [{ email }];
+  sendSmtpEmail.sender = { email: "alihasan666.co@gmail.com", name: "App" };
+  sendSmtpEmail.subject = "Reset Password OTP";
+  sendSmtpEmail.htmlContent = `
+    <h2>Reset Password</h2>
+    <p>الـ OTP بتاعك هو:</p>
+    <h1 style="color: #4CAF50; letter-spacing: 5px">${otp}</h1>
+    <p>صالح لمدة 10 دقايق بس</p>
+  `;
 
   await apiInstance.sendTransacEmail(sendSmtpEmail);
 };
