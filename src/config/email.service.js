273a3c48f618b9275743c641bf5a08522d5cdc7e
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-02 22:47:59 +0300

    useing pravo liberary3

--- src/config/email.service.js
+++ src/config/email.service.js
@@ -1,22 +1,50 @@
-const SibApiV3Sdk = require("@getbrevo/brevo");
+// const SibApiV3Sdk = require("@getbrevo/brevo");
 
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-apiInstance.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+// const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+// apiInstance.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+
+// const sendOTPEmail = async (email, otp) => {
+//   const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+//   sendSmtpEmail.to = [{ email }];
+//   sendSmtpEmail.sender = { email: "alihasan666.co@gmail.com", name: "App" };
+//   sendSmtpEmail.subject = "Reset Password OTP";
+//   sendSmtpEmail.htmlContent = `
+//     <h2>Reset Password</h2>
+//     <p>الـ OTP بتاعك هو:</p>
+//     <h1 style="color: #4CAF50; letter-spacing: 5px">${otp}</h1>
+//     <p>صالح لمدة 10 دقايق بس</p>
+//   `;
+
+//   await apiInstance.sendTransacEmail(sendSmtpEmail);
+// };
+
+// module.exports = { sendOTPEmail };
+
+const { TransactionalEmailsApi, SendSmtpEmail } = require("@getbrevo/brevo");
+
+const apiInstance = new TransactionalEmailsApi();
+
+apiInstance.authentications.apiKey.apiKey = process.env.BREVO_API_KEY;
 
 const sendOTPEmail = async (email, otp) => {
-  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+  const sendSmtpEmail = new SendSmtpEmail();
 
   sendSmtpEmail.to = [{ email }];
-  sendSmtpEmail.sender = { email: "alihasan666.co@gmail.com", name: "App" };
+  sendSmtpEmail.sender = {
+    email: "alihasan666.co@gmail.com",
+    name: "App",
+  };
+
   sendSmtpEmail.subject = "Reset Password OTP";
+
   sendSmtpEmail.htmlContent = `
     <h2>Reset Password</h2>
     <p>الـ OTP بتاعك هو:</p>
-    <h1 style="color: #4CAF50; letter-spacing: 5px">${otp}</h1>
-    <p>صالح لمدة 10 دقايق بس</p>
+    <h1>${otp}</h1>
   `;
 
-  await apiInstance.sendTransacEmail(sendSmtpEmail);
+  return await apiInstance.sendTransacEmail(sendSmtpEmail);
 };
 
 module.exports = { sendOTPEmail };
