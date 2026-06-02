b78d61d5b99a3fd701aecbe9815ab4545c60c934
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-02 14:12:30 +0300

    fix cros seting 2

--- src/app.js
+++ src/app.js
@@ -19,7 +19,23 @@ const passport = require("passport");
 require("./modules/auth/google.strategy");
 app.use(helmet());
 app.use(globalLimiter); //protection all route
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // السماح لـ Postman والأدوات بدون origin
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(passport.initialize());
