ff4631cf1345709d50fe5543c3064dc813768f00
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-06 01:56:11 +0300

    fex rate limit

--- src/config/rate.limit.js
+++ src/config/rate.limit.js
@@ -1,23 +1,28 @@
 const rateLimit = require("express-rate-limit");
 
+const skipOptions = (req) => req.method === "OPTIONS"; // ✅ استثني الـ preflight
+
 // protection all routes
 const globalLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, //15 menuts
-  max: 100, // maxmim of requests
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  skip: skipOptions, // ✅
   message: { message: "Too many requests, please try again later" },
 });
 
 // protection auth route
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, //15 menuts
-  max: 10, // maxmim of requests
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  skip: skipOptions, // ✅
   message: { message: "Too many login attempts, please try again later" },
 });
 
 // protection send otp
 const otpLimiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 3, // 3 محاولات بس
+  max: 3,
+  skip: skipOptions, // ✅
   message: { message: "Too many OTP requests, please try again after an hour" },
 });
 
