ca40c5052d6b937ae2d55dd0a87a9f9c1f43bc09
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-06 23:23:20 +0300

    fex refresh token 2

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -29,8 +29,8 @@ const loginController = async (req, res) => {
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
