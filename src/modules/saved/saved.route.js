1663e10c99b119c86da0cd3ff7133a4856dc6591
Ebraheem hany <ebraheem200412345@gmail.com>
2026-06-10 23:24:31 +0300

    fix saved router

--- src/modules/saved/saved.route.js
+++ src/modules/saved/saved.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const route = express.Router();
+
 const {
   toggleSavePostController,
   getSavedPostsController,
@@ -9,4 +10,4 @@ const authMiddleware = require("../../middleware/auth.middleware");
 route.post("/posts/:postId/save", authMiddleware, toggleSavePostController);
 route.get("/saved-posts", authMiddleware, getSavedPostsController);
 
-module.exports = router;
+module.exports = route;
